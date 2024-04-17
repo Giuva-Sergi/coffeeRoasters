29a930a1120ddbe22cb69f4338f54201851e70ce
Bruno Moleta <brunomoleta@pm.me>
2024-04-17 16:50:53 -0300

    Add Balancer to improve text wrapping
    The changes include addition of the `react-wrap-balancer` component to different sections across the 'About' pages. This helps in better text wrapping for the description paragraphs and enhanced visual appeal, primarily aiming to improve user readability and site aesthetics.

--- src/pages/about/Quality.jsx
+++ src/pages/about/Quality.jsx
@@ -2,23 +2,27 @@ import styles from "./Quality.module.css";
 import qualityDesktop from "../../assets/about/desktop/image-quality.jpg";
 import qualityTablet from "../../assets/about/tablet/image-quality.jpg";
 import qualityMobile from "../../assets/about/mobile/image-quality.jpg";
+import Balancer from "react-wrap-balancer";
+
 function Quality() {
   return (
     <section className={styles.quality}>
       <div className={styles.description}>
         <h3>Uncompromising quality</h3>
         <p>
-          Although we work with growers who pay close attention to all stages of
-          harvest and processing, we employ, on our end, a rigorous quality
-          control program to avoid over-roasting or baking the coffee dry. Every
-          bag of coffee is tagged with a roast date and batch number. Our goal
-          is to roast consistent, user-friendly coffee, so that brewing is easy
-          and enjoyable.
+          <Balancer>
+            Although we work with growers who pay close attention to all stages
+            of harvest and processing, we employ, on our end, a rigorous quality
+            control program to avoid over-roasting or baking the coffee dry.
+            Every bag of coffee is tagged with a roast date and batch number.
+            Our goal is to roast consistent, user-friendly coffee, so that
+            brewing is easy and enjoyable.
+          </Balancer>
         </p>
       </div>
       <picture>
-        <source media="(max-width: 728px)" srcset={qualityMobile} />
-        <source media="(max-width: 1140px)" srcset={qualityTablet} />
+        <source media="(max-width: 728px)" srcSet={qualityMobile} />
+        <source media="(max-width: 1140px)" srcSet={qualityTablet} />
         <img
           className={styles.picture}
           src={qualityDesktop}
